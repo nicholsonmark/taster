1f523a815a38dd30b53ea62fbc197c08cd642595
Mark Nicholson <mnicholson@travellianceinc.com>
2017-01-02 19:56:29 -0600

    inital commit

---
+++ taster/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+
+namespace taster.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FirstAjax(string a)
+        {
+            return Json("chamara", JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
